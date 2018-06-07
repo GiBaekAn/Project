d8b5e0300ffa1ec4f48dbb61c83e44058310410f
AnGiBaek <akb0516@ajou.ac.kr>
2018-06-07 10:23:25 +0900

    MainPage XML Layout 구성 Picasso를 사용

--- app/src/main/java/com/example/akb05/project/Mainpage.java
+++ app/src/main/java/com/example/akb05/project/Mainpage.java
@@ -1,5 +1,6 @@
 package com.example.akb05.project;
 
+import android.*;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,7 +9,9 @@ import android.view.SoundEffectConstants;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.GlideException;
@@ -22,6 +25,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 /**
  * Created by dong on 2018-05-21.
@@ -35,6 +43,7 @@ public class Mainpage extends FragmentActivity implements OnMapReadyCallback{
 
     Uri a = Uri.parse(storageReference.toString());
 
+    ImageView test;
     ImageButton menuimg;
     Button inputmenubtn;
     // 오버레이 관리자
@@ -46,12 +55,32 @@ public class Mainpage extends FragmentActivity implements OnMapReadyCallback{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+//
+//        PermissionListener permissionlistener = new PermissionListener() {
+//            @Override
+//            public void onPermissionGranted() {
+//                Toast.makeText(Mainpage.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+//                Toast.makeText(Mainpage.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//        new TedPermission(this)
+//                .setPermissionListener(permissionlistener)
+//                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission] ")
+//                .setPermissions(android.Manifest.permission.INTERNET)
+//                .check();
+
 
         MapContainer = (LinearLayout) findViewById(R.id.mapcontainer);
         menuimg = findViewById(R.id.menuimg);
         inputmenubtn = findViewById(R.id.inputmenubtn);
+        test = findViewById(R.id.test);
 
-        //Glide.with(this).load(a).into(menuimg);
+        Picasso.get().load(a).error(R.drawable.err).into(test);
+        //Glide.with(this).load(a).into(test);
         //Glide.with(getBaseContext()).load(a).into(menuimg);
 
         menuimg.setOnClickListener(new View.OnClickListener() {
